513603bffd96133d89309c687ab2c2e0a9879663
Navyashree K N <121098180+navya8722@users.noreply.github.com>
2025-01-02 22:37:34 +0530

    Create AdminDashboard.js
    Create an AdminDashboard.js to manage the entire admin module.

---
+++ AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CompanyManagement from './CompanyManagement';
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <CompanyManagement />
+    </div>
+  );
+};
+
+export default AdminDashboard;
